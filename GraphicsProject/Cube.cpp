34cd4d9237a6eae519fbe90270ffa616fe2df50a
Lucas <s208055>
2021-10-14 14:41:35 -0500

    rearranged cube cpp  and got rid of unnecessary content

--- GraphicsProject/Cube.cpp
+++ GraphicsProject/Cube.cpp
@@ -2,47 +2,33 @@
 
 Cube::Vertex* Cube::generateVertices(unsigned int& vertexCount)
 {
-#pragma region normals
-	glm::vec4 forwardNormal = { 0.0f,0.0f,1.0f,0.0f };
-	glm::vec4 rightNormal = { 1.0f,0.0f,0.0f,0.0f };
-	glm::vec4 upNormal = { 0.0f, 1.0f, 0.0f, 0.0f };
-#pragma endregion
-
-	Vertex* verticies = nullptr;
+	Vertex* vertices = nullptr;
 	vertexCount = 8;
-	verticies = new Vertex[vertexCount];
+	vertices = new Vertex[vertexCount];
 
-	verticies[0].position = { -1.0f,1.0f,-1.0f,1.0f };//a
-	verticies[1].position = { -1.0f,1.0f,1.0f,1.0f };//b
-	verticies[2].position = { 1.0f,1.0f,-1.0f,1.0f };//c
-	verticies[3].position = { 1.0f,1.0f,1.0f,1.0f };//d
-	verticies[4].position = { 1.0f,-1.0f,1.0f,1.0f };//e
-	verticies[5].position = { 1.0f,-1.0f,-1.0f,1.0f };//f
-	verticies[6].position = { -1.0f,-1.0f,1.0f,1.0f };//g
-	verticies[7].position = { -1.0f,-1.0f,-1.0f,1.0f };//h
+	vertices[0].position = { -1.0f,1.0f,-1.0f,1.0f };//a
+	vertices[1].position = { -1.0f,1.0f,1.0f,1.0f };//b
+	vertices[2].position = { 1.0f,1.0f,-1.0f,1.0f };//c
+	vertices[3].position = { 1.0f,1.0f,1.0f,1.0f };//d
 
-	verticies[0].normal = { -1.0f,1.0f,-1.0f,0.0f };//a
-	verticies[1].normal = { -1.0f,1.0f,1.0f,0.0f };//b
-	verticies[2].normal = { 1.0f,1.0f,-1.0f,0.0f };//c
-	verticies[3].normal = { 1.0f,1.0f,1.0f,0.0f };//d
-	verticies[4].normal = { 1.0f,-1.0f,1.0f,0.0f };//e
-	verticies[5].normal = { 1.0f,-1.0f,-1.0f,0.0f };//f
-	verticies[6].normal = { -1.0f,-1.0f,1.0f,0.0f };//g
-	verticies[7].normal = { -1.0f,-1.0f,-1.0f,0.0f };//h
+	vertices[4].position = { 1.0f,-1.0f,1.0f,1.0f };//e
+	vertices[5].position = { 1.0f,-1.0f,-1.0f,1.0f };//f
+	vertices[6].position = { -1.0f,-1.0f,1.0f,1.0f };//g
+	vertices[7].position = { -1.0f,-1.0f,-1.0f,1.0f };//h
+
+	vertices[0].normal = { -1.0f,1.0f,-1.0f,0.0f };//a
+	vertices[1].normal = { -1.0f,1.0f,1.0f,0.0f };//b
+	vertices[2].normal = { 1.0f,1.0f,-1.0f,0.0f };//c
+	vertices[3].normal = { 1.0f,1.0f,1.0f,0.0f };//d
+	vertices[4].normal = { 1.0f,-1.0f,1.0f,0.0f };//e
+	vertices[5].normal = { 1.0f,-1.0f,-1.0f,0.0f };//f
+	vertices[6].normal = { -1.0f,-1.0f,1.0f,0.0f };//g
+	vertices[7].normal = { -1.0f,-1.0f,-1.0f,0.0f };//h
 
 	for (int i = 0; i < vertexCount; i++) {
-		verticies[i].color = m_color;
+		vertices[i].color = m_color;
 	}
-	return verticies;
-}
-
-void Cube::setVertexPosition(Vertex* verticies, int vertexCount, int index, glm::vec3 position)
-{
-	for (int i = index; i < vertexCount; i += 8) {
-		verticies[i].position = glm::vec4(position * 0.5f,1.0f);
-		verticies[i].normal = glm::vec4(position, 0.0f);
-		verticies[i].color = m_color;
-	}
+	return vertices;
 }
 
 unsigned int* Cube::generateIndices(unsigned int& indexCount)
@@ -53,14 +39,19 @@ unsigned int* Cube::generateIndices(unsigned int& indexCount)
 	unsigned int* indices = new unsigned int[indexCount] {
 		0, 1, 2,
 		3, 1, 2,
+
 		0, 1, 7,
 		6, 1, 7,
+
 		0, 2, 5,
 		0, 7, 5,
+
 		2, 3, 4,
 		2, 4, 5,
+
 		4, 5, 7,
 		4, 7, 6,
+
 		3, 1, 6,
 		3, 6, 4
 	};
--- GraphicsProject/Cube.h
+++ GraphicsProject/Cube.h
@@ -5,12 +5,12 @@ class Cube : public Mesh
 public:
 	Cube() : Mesh() {}
 	~Cube();
-	Vertex* generateVertices(unsigned int& vertexCount) override;
 
 	glm::vec4 getColor() { return m_color; }
 	void setColor(glm::vec4 color) { m_color = color; }
+
 private:
-	void setVertexPosition(Vertex* verticies,int vertexCount, int index, glm::vec3 position);
+	Vertex* generateVertices(unsigned int& vertexCount) override;
 	unsigned int* generateIndices(unsigned int& indexCount) override;
 private:
 	glm::vec4 m_color = glm::vec4(1.0f);
